674556f097c86e94db2e35fbfe3a90e3b47ef4ff
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-04-05 16:38:32 +0000

    Password Generator

---
+++ app.js
@@ -0,0 +1,29 @@
+/* 
+uppercases
+lowercases
+numbers
+symbols
+size
+*/
+let passGenerated = [];
+const generatePassword = (hasUppercases = false, hasLowercases = true, hasNumbers = false, hasSymbols = false, size = 8) => {
+    let password = [];
+    let characters = "";
+
+    if (hasUppercases) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (hasLowercases) characters += "abcdefghijklmnopqrstuvwxyz";
+    if (hasNumbers) characters += "0123456789";
+    if (hasSymbols) characters += "?*%_$@#.";
+
+    let i = 0;
+    while(i < size){
+        password.push(characters.charAt(Math.floor(Math.random() * characters.length)));
+        i++;
+    }
+    passGenerated.push(password.join(''))
+    return password.join('');
+}
+
+let pass =  generatePassword(true, true, true, true, 18);
+console.log(pass);
+console.log(passGenerated);
